ecd22aed4fdbd4d9286368195d205e52e8c514fa
lais-bertoni <laismessiasbertoni@hotmail.com>
2019-04-20 18:24:11 -0300

    Initial commit

---
+++ src/main/java/getninjas/roverchallenge/RoverchallengeApplication.java
@@ -0,0 +1,13 @@
+package getninjas.roverchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoverchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoverchallengeApplication.class, args);
+	}
+
+}
